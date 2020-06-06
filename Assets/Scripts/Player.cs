befaedb334be46ce38069c26fd54d43f5486cdbb
Cesar Killabunnies <killabunnies.cesar@gmail.com>
2020-06-06 20:52:12 -0300

    a

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -44,37 +44,51 @@ public class Player : MonoBehaviour
 	public void updateLegs(List<GameObject> inputPositions)
 	{
 
+		//remove current childrens
+		foreach(Transform leg0Child in legs[0]){
+			sphereToPool(leg0Child.gameObject);
+		}
+		legs[0].DetachChildren();
+		foreach(Transform leg1Child in legs[1]){
+			sphereToPool(leg1Child.gameObject);
+		}
+		legs[1].DetachChildren();
+
+		SphereCollider[] spheres = legs[0].GetComponents<SphereCollider>();
+		foreach(SphereCollider SC in spheres){
+			Destroy(SC);
+		}
+		spheres = legs[1].GetComponents<SphereCollider>();
+		foreach(SphereCollider SC in spheres){
+			Destroy(SC);
+		}
+
 		GameObject first = inputPositions[0];
 		Vector3 firstPosition = first.transform.position;
 		int i = 0;
 		foreach (GameObject inputPosition in inputPositions)
 		{
 			if (i++ == 0) continue;
-			Vector3 position = (inputPosition.transform.position - firstPosition) / Constants.pixelsPerUnitLegs;
+			Vector3 position = (inputPosition.transform.position - firstPosition) / (float)Constants.pixelsPerUnitLegs;
 			GameObject sphere1 = getSphere();
-			Debug.Log("legs[0]: " + legs[0]);
+
 			sphere1.transform.SetParent(legs[0]);
 			sphere1.transform.localPosition = position;
 			GameObject sphere2 = getSphere();
-			Debug.Log("legs[1]: " + legs[1]);
+
 			sphere2.transform.SetParent(legs[1]);
 			sphere2.transform.localPosition = position;
-			//SphereCollider sphereCollider = gameObject.AddComponent<SphereCollider>();
-			//sphereCollider.center = position;
-			//sphereCollider.radius = Constants.sphereLegRadius;
+
+			SphereCollider SC1 = legs[0].gameObject.AddComponent<SphereCollider>();
+			SC1.radius = Constants.sphereLegRadius;
+			SC1.center = position;
+			SphereCollider SC2 = legs[1].gameObject.AddComponent<SphereCollider>();
+			SC2.radius = Constants.sphereLegRadius;
+			SC2.center = position;
+			
 		}
 
-		/*GameObject first = inputPositions[0];
-		Vector3 firstPosition = first.transform.position;
-		int i = 0;
-		foreach (GameObject inputPosition in inputPositions)
-		{
-			if (i++ == 0) continue;
-			Vector3 position = (inputPosition.transform.position - firstPosition) / Constants.pixelsPerUnitLegs;
-			SphereCollider sphereCollider = gameObject.AddComponent<SphereCollider>();
-			sphereCollider.center = position;
-			sphereCollider.radius = Constants.sphereLegRadius;
-		}*/
+		transform.Find("Cube").GetComponent<Rigidbody>().velocity = Vector3.zero;
 	}
 
 	GameObject getSphere()
